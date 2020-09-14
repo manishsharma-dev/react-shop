89d0e9fd4ee7629482fce8e61f89d4620b37d8bf
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-09-14 16:19:38 +0530

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+function navbar() {
+    return (
+        <div>
+            <div className="conatiner">
+            <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+                </div>   
+        </div>
+    )
+}
+
+export default navbar
